7ef6b17040e7d06f4dde7925eba2609b1a78f98f
Ben Alcala <benalcalajr@gmail.com>
2022-07-29 18:20:01 -0700

    create new file for mongo connect and disconnect and update connections to it from server and test files

--- server/src/routes/launches/launches.test.js
+++ server/src/routes/launches/launches.test.js
@@ -1,74 +1,91 @@
 const request = require('supertest');
+
 const app = require('../../app');
+const {mongoConnect, mongoDisconnect} = require('../../utils/mongo');
 
 
-describe('Test /GET launches', () => {
-  test('returns 200 ok', async() => {
-    await request(app)
-    .get('/launches')
-    .expect('Content-Type', /json/)
-    .expect(200)
+describe('Launches API', ()=> {
+  beforeAll(async()=> {
+    await mongoConnect()
   })
+
+  afterAll(async()=> {
+    await mongoDisconnect()
+  })
+
+  describe('Test /GET launches', () => {
+    test('returns 200 ok', async() => {
+      await request(app)
+      .get('/launches')
+      .expect('Content-Type', /json/)
+      .expect(200)
+    })
+  
+  })
+  
+  describe('Test /POST launches', () => {
+    const launchData = {
+      mission: 'Kepler Enterprise',
+      rocket: 'NCC 1701-D',
+      target: 'Kepler-62 f',
+      launchDate: 'January 1, 2030'
+    }
+  
+    const launchDataWithoutDate = {
+      mission: 'Kepler Enterprise',
+      rocket: 'NCC 1701-D',
+      target: 'Kepler-62 f'
+    }
+  
+    const launchDataWithInvalidDate = {
+      mission: 'Kepler Enterprise',
+      rocket: 'NCC 1701-D',
+      target: 'Kepler-62 f',
+      launchDate: 'invalid',
+    }
+  
+    test('returns 201 created', async() => {
+      const response = await request(app)
+      .post('/launches')
+      .send(launchData)
+      .expect('Content-Type', /json/)
+      .expect(201)
+  
+      const responseDate = new Date(response.body.launchDate).valueOf()
+      const requestDate = new Date(launchData.launchDate).valueOf();
+  
+      expect(responseDate).toBe(requestDate)
+      expect(response.body).toMatchObject(launchDataWithoutDate)
+    })
+  
+    test('returns 400 bad request when launch date is not included', async() => {
+      const response = await request(app)
+      .post('/launches')
+      .send(launchDataWithoutDate)
+      .expect(400)
+  
+      expect(response.body).toStrictEqual({
+        error: 'missing launch property'
+      })
+    })
+  
+    test('returns 400 bad request when launch date is invalid', async() => {
+      const response = await request(app)
+      .post('/launches')
+      .send(launchDataWithInvalidDate)
+      .expect(400)
+  
+      expect(response.body).toStrictEqual({
+        error: 'invalid launch date'
+      })
+    })
+  
+  
+  
+  })
+
+
 
 })
 
-describe('Test /POST launches', () => {
-  const launchData = {
-    mission: 'Kepler Enterprise',
-    rocket: 'NCC 1701-D',
-    target: 'Kepler-188 AF',
-    launchDate: 'January 1, 2030'
-  }
 
-  const launchDataWithoutDate = {
-    mission: 'Kepler Enterprise',
-    rocket: 'NCC 1701-D',
-    target: 'Kepler-188 AF'
-  }
-
-  const launchDataWithInvalidDate = {
-    mission: 'Kepler Enterprise',
-    rocket: 'NCC 1701-D',
-    target: 'Kepler-188 AF',
-    launchDate: 'invalid',
-  }
-
-  test('returns 201 created', async() => {
-    const response = await request(app)
-    .post('/launches')
-    .send(launchData)
-    .expect('Content-Type', /json/)
-    .expect(201)
-
-    const responseDate = new Date(response.body.launchDate).valueOf()
-    const requestDate = new Date(launchData.launchDate).valueOf();
-
-    expect(responseDate).toBe(requestDate)
-    expect(response.body).toMatchObject(launchDataWithoutDate)
-  })
-
-  test('returns 400 bad request when launch date is not included', async() => {
-    const response = await request(app)
-    .post('/launches')
-    .send(launchDataWithoutDate)
-    .expect(400)
-
-    expect(response.body).toStrictEqual({
-      error: 'missing launch property'
-    })
-  })
-
-  test('returns 400 bad request when launch date is invalid', async() => {
-    const response = await request(app)
-    .post('/launches')
-    .send(launchDataWithInvalidDate)
-    .expect(400)
-
-    expect(response.body).toStrictEqual({
-      error: 'invalid launch date'
-    })
-  })
-
-
-
-})
--- server/src/server.js
+++ server/src/server.js
@@ -4,25 +4,20 @@ const mongoose = require('mongoose');
 require('dotenv').config();
 
 const app = require('./app');
+const {mongoConnect} = require('./utils/mongo');
 
 const {loadPlanetsData} = require('./models/planets.model')
 
 const PORT = process.env.PORT || 8000;
 
-const MONGO_URL = process.env.MONGO_URL;
+
 
 const server = http.createServer(app);
 
-mongoose.connection.once('open', () => {
-  console.log('MongoDB connection ready!')
-})
 
-mongoose.connection.on('error', (err) => {
-  console.log(err)
-})
 
 const startServer = async() => {
-  await mongoose.connect(MONGO_URL)
+  await mongoConnect()
   await loadPlanetsData();
   server.listen(PORT, ()=> {
     console.log(`Listening on port ${PORT}...`)
